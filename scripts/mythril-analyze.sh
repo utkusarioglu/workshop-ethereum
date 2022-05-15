0fec252675c4271b2ed6e51ac41aea28fc1a018c
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-05-15 09:17:58 +0000

    Chore: attempt at fixing mythril gh actions issue #4
    scripts/mythril-analyze.sh
    - Refactor the `null` check before `myth` runs. This is done
      to tidy up the code, it has nothing to do with the bug.
    - List contracts that are visible to the script file. This
      is the first step in discovering what's wrong with the runs.

--- scripts/mythril-analyze.sh
+++ scripts/mythril-analyze.sh
@@ -7,15 +7,21 @@ source /opt/venv/mythril/bin/activate
 # B, then jq will return `null` for its bytecode and the contract
 # will not be scanned
 CONTRACTS=$(jq -r '.sources | keys[] | gsub(".sol";"")' solc.json)
+
+echo "Contracts:"
+echo $CONTRACTS
+
 for CONTRACT in $CONTRACTS;
 do 
   FILE="$CONTRACT.sol"
   BYTECODE=$(\
     solc --standard-json solc.json --allow-paths $(pwd) | \
     jq -r '.contracts["'$FILE'"].'$CONTRACT'.evm.bytecode.object')
-  if [ $BYTECODE != "null" ]
+  if [ "$BYTECODE" == "null" ]
   then 
-    echo "Analyzing $CONTRACT..."
-    myth analyze -c $BYTECODE
+    echo "Skipping $CONTRACT as there is no bytecode to analyze"
+    continue
   fi
+  echo "Analyzing $CONTRACT..."
+  myth analyze -c $BYTECODE
 done
